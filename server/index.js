82868ec478e00bb26d15dcdf3622aa7daf26af54
yusufaksu <yusuf.aksu@tsoft.com.tr>
2022-05-23 11:31:51 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import memoryRouter from './routers/memoryRouter.js'
+ 
+dotenv.config()
+
+const app = express()
+
+app.use(express.json({limit: '20mb'}))
+
+app.use('/memories', memoryRouter)
+
+app.get('/', (req, res) => {
+    res.json({message:"get isteği  "})
+})
+
+
+app.listen(process.env.PORT, () => {
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    }).then(() => {
+        console.log("connected to db ")
+    }).catch((error) => {
+        console.log(error)
+    })
+})
